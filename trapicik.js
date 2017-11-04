e5dc1cf2ae7a43a7e36133bddbf05a15b8e1aa6e
PeterisP <peterisp@gmail.com>
2017-11-04 18:45:00 +0200

    Writing updates to the google sheet

--- trapicik.js
+++ trapicik.js
@@ -6,12 +6,15 @@ var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"
 var SCOPES = "https://www.googleapis.com/auth/spreadsheets";
 var SHEET_ID;
 var SHEET_NAME = "Dalībnieki";
-var active_row = 1;
+var round_participants = [];
+var active_participant;
+var round;
 var series = 1;
 var hits;
 
 var authorizeButton;
 var signoutButton;
+var writeButton;
 
 /** from StackOverflow https://stackoverflow.com/a/2880929 **/
 var urlParams;
@@ -33,8 +36,10 @@ var urlParams;
 function handleClientLoad() {
   authorizeButton = document.getElementById('authorize-button');
   signoutButton = document.getElementById('signout-button');
+  writeButton = document.getElementById('write-button');
   hits = [0,0,0,0,0,0,0,0,0,0,0,0];
   SHEET_ID = urlParams["sheet"];
+  round = urlParams["round"];
   gapi.load('client:auth2', initClient);
 }
 
@@ -54,12 +59,48 @@ function decrement(tbl, row) {
 function setupTableEvents() {
   var tbl = document.getElementById("hits");
   if (tbl != null) {
-      for (var row = 0; row < tbl.rows.length; row++) {
-        tbl.rows[row].cells[0].onclick = (decrement(tbl, row));
-        tbl.rows[row].cells[1].onclick = (increment(tbl, row));
-        tbl.rows[row].cells[2].onclick = (increment(tbl, row));
-      }
+    for (var row = 0; row < tbl.rows.length; row++) {
+      tbl.rows[row].cells[2].innerHTML = hits[row];
+      tbl.rows[row].cells[0].onclick = (decrement(tbl, row));
+      tbl.rows[row].cells[1].onclick = (increment(tbl, row));
+      tbl.rows[row].cells[2].onclick = (increment(tbl, row));
+    }
+    writeButton.style.display = 'block';
   }
+}
+function writeHits() {
+  var nr = round_participants[active_participant];
+  var range;
+  switch (series) {
+    case 1:
+      range = SHEET_NAME+'!N'+(nr+3)+':Y'+(nr+3)
+      break;
+    case 2:
+      range = SHEET_NAME+'!Z'+(nr+3)+':AK'+(nr+3)
+      break;
+    case 3:
+      range = SHEET_NAME+'!AL'+(nr+3)+':AW'+(nr+3)
+      break;
+    default:
+      return;
+  }
+
+  var values = [];
+  for (i=0;i<12;i++)
+    values[i] = hits[11-i];
+  console.log(values);
+  console.log(range);
+  var body = {values:[values]};
+  console.log(body);
+
+  gapi.client.sheets.spreadsheets.values.update({
+    spreadsheetId: SHEET_ID,
+    range: range,
+    resource: body,
+    valueInputOption: "RAW"
+  }).then(function(response){
+    console.log(response);
+  })
 }
 
 /**
@@ -80,6 +121,7 @@ function initClient() {
     updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
     authorizeButton.onclick = handleAuthClick;
     signoutButton.onclick = handleSignoutClick;
+    writeButton.onclick = writeHits;
   });
 }
 
@@ -91,7 +133,7 @@ function updateSigninStatus(isSignedIn) {
   if (isSignedIn) {
     authorizeButton.style.display = 'none';
     signoutButton.style.display = 'block';
-    updateInfo();
+    fetchRound(round);
   } else {
     authorizeButton.style.display = 'block';
     signoutButton.style.display = 'none';
@@ -124,19 +166,49 @@ function appendPre(message) {
   pre.appendChild(textContent);
 }
 
-function updateInfo() {
-
+function fetchParticipant() {
+  var nr = round_participants[active_participant];
   gapi.client.sheets.spreadsheets.values.get({
     spreadsheetId: SHEET_ID,
-    range: SHEET_NAME+'!A'+(active_row+1)+':AT'+(active_row+1),
+    range: SHEET_NAME+'!A'+(nr+3)+':AW'+(nr+3),
+  }).then(function(response) {
+    var range = response.result;
+    if (range.values.length == 1) {
+      var row = range.values[0];
+      appendPre(row[0] + ', ' + row[1] + ', ' + row[2] + ', ' + row[3] + ', ' + row[4]);
+      for (var i = 0; i < 12; i++) {
+        hit = row[i+13];
+        if (hit=="") {
+          hits[11-i] = 0;
+        } else { 
+          hits[11-i] = parseInt(hit,10);
+        }
+      }
+      setupTableEvents();
+    } else {
+      appendPre('No data found.');
+    }
+  }, function(response) {
+    appendPre('Error: ' + response.result.error.message);
+  });
+}
+
+function fetchRound(round) {
+  round_participants = [];
+  gapi.client.sheets.spreadsheets.values.get({
+    spreadsheetId: SHEET_ID,
+    range: SHEET_NAME+'!A3:E',
   }).then(function(response) {
     var range = response.result;
     if (range.values.length > 0) {
-      for (i = 0; i < range.values.length; i++) {
+      for (var i = 0; i < range.values.length; i++) {
         var row = range.values[i];
-        appendPre(row[0] + ', ' + row[1] + ', ' + row[2] + ', ' + row[3] + ', ' + row[4]);
+        if (row[0]==round) {
+          round_participants.push(i);
+        }
       }
-      setupTableEvents();
+      active_participant=0;
+      fetchParticipant();
     } else {
       appendPre('No data found.');
     }
